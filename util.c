1119a9108e49a996bfcc55dbd8581fd8f1c5a0e7
gift <giftmoyo23@gmail.com>
2023-06-22 14:46:23 -0700

    cool

---
+++ util.c
@@ -0,0 +1,34 @@
+#include <ctype.h>
+#include "monty.h"
+
+/**
+checks if a string is a digit
+*/ 
+int is_digit(char *string)
+{
+	if (!string || *string == '\0')
+		return (0);
+	if (*string == '-')
+		string++;
+	while (*string)
+	{
+		if (isdigit(*string) == 0)
+			return (0);
+		string++;
+	}
+	return (1);
+}
+
+int isnumber(char *str)
+{
+	int i;
+
+	if (!str)
+		return (0);
+
+	for (i = 0; str[i]; i++)
+		if (i < '0' || i > '9')
+			return (0);
+
+	return (1);
+}
