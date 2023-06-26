13046ae600023dfa82ff916b69bbfc6f8d4762fa
Gentletouch1 <adeyemishobukola@gmail.com>
2023-06-25 21:49:55 -0700

    monty_fuction_1.c

--- monty_function_1.c
+++ monty_function_1.c
@@ -7,7 +7,7 @@ void monty_pop(stack_t **stack, unsigned int line_number);
 void monty_swap(stack_t **stack, unsigned int line_number);
 
 /**
- * monty_push - Pushes a value to the stack_t linked list.
+ * monty_push - Pushes a value to a stack_t linked list.
  * @stack: A pointer to the top mode node of a stack_t linked list.
  * @line_number: The current working line number of a Monty bytecodes file.
  */
